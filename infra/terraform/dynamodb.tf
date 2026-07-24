cb13c5be559a22f7fe0bcd579c954a835aa0db2e
supersom <banerjee.somdutta@gmail.com>
2026-07-23 18:57:19 -0700

    Import existing DynamoDB tables into Terraform

---
+++ infra/terraform/dynamodb.tf
@@ -0,0 +1,44 @@
+resource "aws_dynamodb_table" "tenants" {
+  name         = "CustomerSupportAgent-Tenants"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "tenantId"
+
+  attribute {
+    name = "tenantId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "users" {
+  name         = "CustomerSupportAgent-Users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  attribute {
+    name = "tenantId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "email-index"
+    hash_key        = "email"
+    range_key       = "tenantId"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "tenantId-index"
+    hash_key        = "tenantId"
+    projection_type = "ALL"
+  }
+}
