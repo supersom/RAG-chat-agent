6287a62ddf0baeab8d50d91616412261e1721648
supersom <banerjee.somdutta@gmail.com>
2026-07-23 19:08:52 -0700

    Import existing Bedrock Guardrail into Terraform

---
+++ infra/terraform/bedrock_guardrail.tf
@@ -0,0 +1,43 @@
+resource "aws_bedrock_guardrail" "default" {
+  name                      = "customer-support-agent-default"
+  blocked_input_messaging   = "Sorry, I can't help with that request."
+  blocked_outputs_messaging = "Sorry, I can't provide that response. Let me know if there's something else I can help with."
+
+  content_policy_config {
+    filters_config {
+      type            = "VIOLENCE"
+      input_strength  = "MEDIUM"
+      output_strength = "MEDIUM"
+    }
+    filters_config {
+      type            = "PROMPT_ATTACK"
+      input_strength  = "MEDIUM"
+      output_strength = "NONE"
+    }
+    filters_config {
+      type            = "MISCONDUCT"
+      input_strength  = "MEDIUM"
+      output_strength = "MEDIUM"
+    }
+    filters_config {
+      type            = "HATE"
+      input_strength  = "MEDIUM"
+      output_strength = "MEDIUM"
+    }
+    filters_config {
+      type            = "SEXUAL"
+      input_strength  = "MEDIUM"
+      output_strength = "MEDIUM"
+    }
+    filters_config {
+      type            = "INSULTS"
+      input_strength  = "MEDIUM"
+      output_strength = "MEDIUM"
+    }
+  }
+}
+
+resource "aws_bedrock_guardrail_version" "v1" {
+  guardrail_arn = aws_bedrock_guardrail.default.guardrail_arn
+  description   = "Initial published version"
+}
